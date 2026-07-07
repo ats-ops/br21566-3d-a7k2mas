969786e26083b360811dc5dc96c3507110140796
ats-ops <atsukai.1712@gmail.com>
2026-07-07 14:24:01 +0900

    Update specimen4127.js

--- js/config/specimen4127.js
+++ js/config/specimen4127.js
@@ -99,7 +99,7 @@ export const specimen4127 = {
 
             name:"鼻骨",
 
-            position:"-2.67 42.0 130.5",
+            position:"-1.67 42.0 132.5",
 
             text:
             "鼻部を形成する骨。嗅覚器官を支える。"
